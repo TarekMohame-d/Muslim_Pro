0822aa8bd2c60b8b11c47af7bf5a9990b718df01
Tarek Mohamed <tarekmohamed1019@gmail.com>
2022-08-10 17:13:32 +0200

    Initial commit "Base code of project"

---
+++ app/src/main/java/com/example/muslimpro/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.muslimpro;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+// Home Screen
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("القائمة الرئيسية");
+        toolbar.setLogo(R.drawable.home_icon);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.about:
+            {
+                Intent i = new Intent(MainActivity.this, About.class);
+                startActivity(i);
+            }
+            break;
+        }
+        return false;
+    }
+}
